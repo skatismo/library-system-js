06e45e34e82820fe9b111a7aa5cd11c9857bc099
skatismo <bocao.skatismo@gmail.com>
2026-08-22 17:56:09 -0300

    feat: update library system

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ class Livro {
 const livros = [
     new Livro("Dom Casmurro", "Machado de Assis", 184, 2),
     new Livro("1984", "George Orwell", 120, 1),
+    new Livro("Senhor das Armas", "George Clooney", 120, 1),
     new Livro("O Hobbit", "J. R. R. Tolkien", 320, 5),
     new Livro("Senhor dos Anéis", "J. R. R. Tolkien", 540, 0)
 ];
@@ -53,10 +54,65 @@ function localizarLivro(livros, tituloBusca) {
     );
 }
 
-// ======================================
+function listarLivrosDisponiveis(livros) {
+    return livros.filter(
+        livro => livro.estaDisponivel()
+    );
+}
+
+function listarTitulosDisponiveis(livros) {
+    const disponiveis = listarLivrosDisponiveis(livros);
+    return disponiveis.map(
+        livro => livro.titulo
+    );
+}
+
+function calcularTotalExemplares(livros) {
+    return livros.reduce((acumulador, livro) => {
+        return acumulador + livro.quantidade;
+    }, 0);
+}
+
+function listarLivrosPorAutor(livros, autorBusca) {
+    return livros.filter(
+        livro => livro.autor === autorBusca
+    );
+}
+
+function buscarLivrosPorTitulo(livros, busca) {
+    const buscaNormalizada = busca.toLowerCase();
+
+    return livros.filter(
+        livro => livro.titulo.toLowerCase().includes(buscaNormalizada)
+    );
+}
+
+//ESTOU AQUI
+function realizarEmprestimo(livros, tituloBusca) {
+    const encontrarLivro = localizarLivro(livros, tituloBusca);
+
+    if (encontrarLivro) {
+        return encontrarLivro.fazerEmprestimo();
+    } else {
+        return "Livro não encontrado.";
+    }
+}
+
+
+
+
+const titulos = listarTitulosDisponiveis(livros);
+
+const total = calcularTotalExemplares(livros);
+
+
+
+
+
+// =====================================
 // Simulação de uso do sistema
 // ======================================
-const livro = localizarLivro(livros, "1984");
+/*const livro = localizarLivro(livros, "1984");
 
 if (livro) {
     console.log(livro.obterResumo());
@@ -66,4 +122,26 @@ if (livro) {
     console.log(livro.obterResumo());
 } else {
     console.log("Livro não encontrado.");
-}
+}*/
+
+/*console.log(titulos);
+
+console.log(total);
+
+const livrosTolkien = listarLivrosPorAutor(
+    livros,
+    "J. R. R. Tolkien"
+);
+
+console.log(livrosTolkien);
+
+
+console.log(
+    listarLivrosPorAutor(livros, "Clarice Lispector")
+);*/
+
+console.log(realizarEmprestimo(livros, "1984"));
+
+console.log(realizarEmprestimo(livros, "1984"));
+
+console.log(realizarEmprestimo(livros, "Harry Potter"));
